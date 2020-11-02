e7dcd0a810a18942316c9cc77cfbf0bb9279ef98
Stefan Pauka <derfrodo0715@gmail.com>
2020-11-02 07:23:47 +0100

    extend clean

--- cleanForPublish.js
+++ cleanForPublish.js
@@ -1,6 +1,12 @@
 const fs = require("fs-extra");
 
 fs.remove("./src");
+if (fs.existsSync("./.git")) {
+    fs.remove("./.git");
+}
+if (fs.existsSync("./.github")) {
+    fs.remove("./.github");
+}
 fs.remove("./.vscode");
 fs.remove("./node_modules");
 fs.remove("clean.js");
